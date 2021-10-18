bf0863e8b080fed7c7892c4794039cbf4b6401ae
freedeenn <freedeenn@gmail.com>
2021-10-18 17:00:28 +0200

    cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,28 +1,98 @@
-document.querySelector('.number').innerHTML = localStorage.getItem('number') || 0;
+// document.querySelector('.number').innerHTML = localStorage.getItem('number') || 0;
 
-function cartPage (kanap){
+const addToCart = document.querySelectorAll(".addToCart");
+const cart = document.querySelector("cart");
+const cartQuantity = document.querySelector("cartQuantity");
+const fullPrice = document.querySelector("fullPrice");
+let price = 0;
+
+const randomId = () => {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+};
+
+const priceWithoutSpaces = (str) => {
+    return str.replace(/\s/g,'');
+};
+
+const normalPrice = (str) => {
+    return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+};
+
+
+
+
+
+
+
+// const cart = querySelector('.cart')
+
+// let article = document.createElement('article')
+// article.id = product.id;
+// article.className = 'cart__item';
+// console.log('cart');
+
+
+// function cartPage (kanap){
 
 // Création d'une balise article, et de 8 div 
-    let article = document.createElement('article');
-    article.id = product.id;
-    article.className = 'cart';
-    let imgCart = document.createElement('div')
-    imgCart.className = 'cart__items'
-    let infoCart = document.createElement('div');
-    infoCart.className = 'kanap-info'
-    console.log(kanap-info)
+//     let article = document.createElement('article');
+//     article.id = product.id;
+//     article.className = 'cart';
+//     let imgCart = document.createElement('div')
+//     imgCart.className = 'cart__items'
+//     let infoCart = document.createElement('div');
+//     infoCart.className = 'kanap-info'
+//     console.log(kanap-info)
+// }
+// function generateProductHTML(kanap) {
+
+//     //Selectionner la div qui va contenir le outerHTML du container
+//     let container = document.querySelector('.cart')
+
+//     //Création de la balise article et ajout d'une classe
+//     let article = document.createElement('article')
+//     article.className = 'kanap-container'
+
+//     //Child de l'article
+//     let image = document.createElement('div');
+//     image.className = 'kanap';
+//     let info = document.createElement('div');
+//     info.className = 'kanap-info';
+//     let a = document.createElement('a');
+//     a.setAttribute('href', `product.html?id=${kanap._id}`);
+
+//     //Child des autres balises    
+//     let img = document.createElement('img');
+//     img.className = 'kanap-pic';
+//     img.setAttribute('src', kanap.imageUrl);
+//     img.setAttribute("alt", `image ${kanap.name}`);
+//     let h2 = document.createElement('h2');
+//     h2.className = 'kanap-name';
+//     h2.innerText = kanap.name;
+//     let p = document.createElement('p');
+//     p.className = 'description';
+//     p.innerText = kanap.description;
+//     let span = document.createElement('span');
+//     span.className = 'price';
+//     span.innerText = `${kanap.price}€`;
 
 
 
+//     image.appendChild(img);
+//     info.appendChild(h2);
+//     info.appendChild(span);
+//     article.appendChild(image);
+//     article.appendChild(info);
+//     article.appendChild(p);
+//     a.appendChild(article);
 
+//     container.appendChild(a);
+// }
 
-
-
-
-
-
-
-
-
-
-}
+// AddToCart.addEventListener("click", function () {
+//     let produits = JSON.parse(localStorage.getItem('article'));
+//     let productIndex = produits.findIndex(function (element) {
+//         return element.id === product.id;
+//     });
+//     console.log(AddToCart)
+// })
--- front/js/product.js
+++ front/js/product.js
@@ -8,12 +8,12 @@ const id = urlParams.get('id');
 function generateProductHTML(kanap) {
   const img = document.createElement("img");
   img.src = `${kanap.imageUrl}`;
-  img.setAttribute("alt",`image ${kanap.name}`);
+  img.setAttribute("alt", `image ${kanap.name}`);
   document.querySelector('.item__img').appendChild(img);
   document.getElementById('title').innerText = `${kanap.name}`;
   document.getElementById('price').innerText = `${kanap.price}`;
   document.getElementById('description').innerText = `${kanap.description}`;
-  
+
   kanap.colors.forEach(color => {
     const option = document.createElement('option');
     option.value = color;
@@ -30,3 +30,78 @@ fetch(`${url}/${id}`)
   .catch(function (error) {
     console.log(error)
   });
+
+  const elt = document.getElementById('addToCart');    // On récupère l'élément sur lequel on veut détecter le clic
+  elt.addEventListener('click', function(event) {      // On écoute l'événement click, notre callback prend un paramètre que nous avons appelé event ici
+      event.preventDefault();                          // On utilise la fonction preventDefault de notre objet event pour empêcher le comportement par défaut de cet élément lors du clic de la souris
+      console.log('click')                            
+  });
+
+  // let cart = {
+  //   'id': 1
+  // };
+
+  // document.onclick = event => {
+  //   if (event.target.classList.contains('plus')) {
+  //     console.log(event.target.dataset.id);
+  //     // plusFunction(event.target.dataset.id);
+  //   }
+  // }
+
+// let span = document.querySelectorById('addToCart');
+// addToCart.addEventListener("click", function () {
+//   console.log(test0)
+//   // console.log(this.closest('.scnd-teddy-container').getAttribute('data-id'));
+//   const img = document.querySelector("img");
+//   img.src = `${kanap.imageUrl}`;
+//   img.setAttribute("alt", `image ${kanap.name}`);
+//   document.querySelector('.item__img').appendChild(img);
+//   document.getElementById('title').innerText = `${kanap.name}`;
+//   let color = document.querySelector('#select').value
+//   document.getElementById('price').innerText = `${kanap.price}`;
+//   document.getElementById('description').innerText = `${kanap.description}`;
+//   let produits = JSON.parse(localStorage.getItem('produits')) || [];
+//   let productIndex = produits.findIndex(function (element) {
+//     console.log(element.price);
+//     return element.id === id;
+//   })
+  // console.log(productIndex)
+  // console.log(produits);
+  // console.log(produits[productIndex]);
+  // if (productIndex === -1) {
+  //   produits.push({
+  //     id: id,
+  //     image: image,
+  //     price: price,
+  //     name: name,
+  //     color: color,
+  //     count: 1
+  //   })
+  // } else {
+  //   produits[productIndex] = {
+  //     id: id,
+  //     image: image,
+  //     price: price,
+  //     name: title,
+  //     color: color,
+  //     count: produits[productIndex].count + 1
+  //   }
+  // }
+//   localStorage.setItem('produits', JSON.stringify(produits))
+
+//   let msgTotal = produits.reduce(function (prev, cur) {
+//     console.log(prev);
+//     console.log(cur);
+//     return prev + cur.count;
+//   }, 0)
+//   console.log(msgTotal);
+//   document.querySelector('.number').innerHTML = msgTotal;
+//   localStorage.setItem('number', msgTotal)
+
+//   alert(name + " (" + color + ") a été ajouté au panier !");
+
+// });
+
+
+
+document.querySelector('.number').innerHTML = localStorage.getItem('number') || 0;
